074651cf8637a986ba863a00725711f9e68c7c08
wayne1098 <wayne10pnd@gmail.com>
2022-06-12 00:57:47 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -6,27 +6,6 @@ function App() {
   return (
     <div className="App">
      <KomponenA />
-
-
-    
-      {/* <header className="App-header">
-       <img src={logo} className="App-logo" alt="logo" />
-       <p>
-        deploy <code>src/App.js</code> and save to reload.
-         
-         <br />
-         deploy to heroku
-        </p>
-       
-       <a
-         className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-         rel="noopener noreferrer"
-       >
-         Learn React
-       </a>
-     </header> */}
     </div>
   );
 }
