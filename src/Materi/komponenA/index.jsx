d451de404a401a98f785446e7ad8a44caa80bf5f
wayne1098 <wayne10pnd@gmail.com>
2022-06-12 00:47:10 +0700

    move

--- src/Materi/komponenA/index.jsx
+++ src/Materi/komponenA/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ClassComponent from ".pembahasan/ClassComponent";
-import FunctionalComponent from ".pembahasan/FunctionalComponent";
+import ClassComponent from "./pembahasan/ClassComponent";
+import FunctionalComponent from "./pembahasan/FunctionalComponent";
 
 export default Class komponenA extends React.Component {
  
