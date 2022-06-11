e0a4e87c26ee3917ae17cf01cc01c3e164dc2da4
wayne1098 <wayne10pnd@gmail.com>
2022-06-11 14:23:02 +0700

    modif

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ function App() {
      <KomponenA />
 
     
-      // <header className="App-header">
-      //   <img src={logo} className="App-logo" alt="logo" />
-      //   <p>
-      //     deploy <code>src/App.js</code> and save to reload.
+      <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
+       <p>
+        deploy <code>src/App.js</code> and save to reload.
+         
+         <br />
+         deploy to heroku
+        </p><a>
           
-      //     <br />
-      //     deploy to heroku
-      //     </p><a>
-          
-      //   </p>
-      //   <a
-      //     className="App-link"
-      //     href="https://reactjs.org"
-      //     target="_blank"
-      //     rel="noopener noreferrer"
-      //   >
-      //     Learn React
-      //   </a>
-      // </header>
+       </p>
+       <a
+         className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+         rel="noopener noreferrer"
+       >
+         Learn React
+       </a>
+     </header>
     </div>
   );
 }
