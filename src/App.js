5361a09ca3f842a92d8a98700d352a6f468da740
wayne1098 <wayne10pnd@gmail.com>
2022-06-12 07:30:28 +0700

    edit app.js ,

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
   return (
     <div className="App">
      <KomponenA />
-    </div>
-  );
-}
-<header className="App-header">
+     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,5 +20,9 @@ function App() {
           Learn React
         </a>
         </header>
+    </div>
+  );
+}
+
        
 export default App;
