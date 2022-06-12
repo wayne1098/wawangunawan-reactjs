86708eb276c826e02c15449d6bdab82cde2bb699
wayne1098 <wayne10pnd@gmail.com>
2022-06-12 07:28:53 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import KomponenA from "./Materi/komponenA";
 
@@ -9,5 +9,19 @@ function App() {
     </div>
   );
 }
-
+<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        </header>
+       
 export default App;
