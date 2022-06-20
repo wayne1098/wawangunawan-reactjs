8b8c23c9e0b93a59e749c970bf6ee78f6bd81139
wayne1098 <wayne10pnd@gmail.com>
2022-06-20 23:04:34 +0700

    insert tugas form

---
+++ src/Materi/styling/index.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+
+// function MyForm() {
+//   return (
+                
+ 
+      
+//       );
+//     }
+    
+//     export default MyForm;
+
