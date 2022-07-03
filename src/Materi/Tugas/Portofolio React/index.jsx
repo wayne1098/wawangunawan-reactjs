eac3de01908c611e7a0b31bb7b5b773cf0bddcf2
wayne1098 <wayne10pnd@gmail.com>
2022-07-03 23:04:23 +0700

    tugas Hook edit4

--- src/Materi/Tugas/Portofolio React/index.jsx
+++ src/Materi/Tugas/Portofolio React/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Card, Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import {Card, Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import "./index.css";
 
 export default class Portofolio extends React.Component {
